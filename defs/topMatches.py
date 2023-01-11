9c5bfab12b1e3470cd4cec352b351358a65dc0d3
Аэлития <53450654+20Aelita03@users.noreply.github.com>
2023-01-11 21:54:19 +0300

    Update topMatches.py

--- defs/topMatches.py
+++ defs/topMatches.py
@@ -1,6 +1,6 @@
 def topMatches(prefs, person, n = 5, similarity = sim_pearson):
     '''Return the best matches for person from the prefs dictionary.'''
-    scores = [(similarity(prefs, person, other), other)
+    scores = [(round(similarity(prefs, person, other), 5), other)
               for other in prefs if other != person]
     # sort the list so the highest scores appear at the top
     scores.sort(reverse = True)
